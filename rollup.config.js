aafaf1924dd140ac877c69e0d1e8eacc89ff0e79
Viktor Hesselbom <viktor@hesselbom.net>
2021-11-24 13:23:18 +0100

    Add extra export to work with react-native's bundler

--- rollup.config.js
+++ rollup.config.js
@@ -8,5 +8,17 @@ export default [
       format: 'cjs',
       sourcemap: true
     }
+  },
+
+  // Add .js to work with react-native
+  {
+    input: 'src/index.js',
+    external: id => /^(lib0|crdtmap|level)/.test(id),
+    output: {
+      name: 'CrdtMapLevelDB',
+      file: 'dist/crdtmap-leveldb.cjs.js',
+      format: 'cjs',
+      sourcemap: true
+    }
   }
 ]
